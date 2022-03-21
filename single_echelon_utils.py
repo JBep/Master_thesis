072b0bfa4250dcc8cc621f055c224344652875c5
Jakob <jakob.bengtsson96@gmail.com>
2022-03-21 10:54:00 +0100

    Update simulation model

--- single_echelon_utils.py
+++ single_echelon_utils.py
@@ -79,3 +79,80 @@ def probability_IL_compound_poisson(R: int,Q: int,demand_probability_array: np.a
         demand_prob_sum = demand_prob_sum + demand_probability_array[k-j]
 
     return 1/Q*demand_prob_sum
+
+def IL_probability_array_compound_poisson(R: int, Q: int, L: int, E_z: float, 
+    V_z: float, compounding_dist: str, positive = 1, threshold = 1e-4) -> np.array:
+    """Computes an array of IL probabilities.
+    
+    Params:
+        R: Reorder point
+        Q: order quantity
+        L: Lead time.
+        E_z: mean demand during a time unit.
+        V_z: variance of demand during a time unit.
+        positive: If 1, returns probabilities of positive IL=j (j=index), 
+            f -1, returns probabilities of negative IL = j (j = -index).
+        threshold: when probability goes below the threshold, all IL above is 
+            assumed to have probability zero.
+
+    Returns:
+        Array of IL probabilities.
+    """
+    if positive != 1 or positive != -1:
+        raise ValueError("Positive needs to be 1 or -1.")
+
+    IL_probabilies_array = []
+    p = 1
+    j = 0
+    while p > threshold:
+        demand_probability_array = demand_probability_array_compound_poisson(L, mu, sigma2)
+        IL_probabilies_array.append(probability_IL_compound_poisson(R,Q,demand_probability_array,j))
+
+def demand_probability_array_compound_poisson(L: int, E_z: float, V_z: float, compounding_dist) -> np.array:
+    pass
+
+def demand_prob_arr_comp_poisson_logarithmic(L: int, E_z: float, V_z: float, threshold = 1e-4):
+    """Computes the array of demand probabilities under logarithmic compound poisson.
+    
+    This function actually uses the fact that logarithmic compound poisson is the 
+    negative binomial distribution.
+
+    reference: Axsäter 2006, Inventory control, p. 81, eq 5.15-5.17
+
+    Params:
+        L: Lead time
+        E_z: Mean demand during one time unit.
+        V_z: Variance of demand during one time unit.
+        threshold: Demand with probability less than this is deemed to be zero.
+    
+    """
+
+    # First find params r and p.
+    p = 1 - (E_z/V_z)
+    r = E_z*(1-p)/p
+  
+    demand_prob_arr = []
+
+    # First do k = 0
+    demand_prob_arr.append((1-p)**r)
+    
+    # Then do for k = 1,2,...
+    p = 1
+    k = 1
+    while p > threshold:
+        P_D_k = 0
+        temp = 0
+        while temp < k:
+            P_D_k *= (r + temp)
+            temp += 1
+
+        P_D_k = (P_D_k / np.math.factorial(k))*((1-p)**r)*(p**k)
+
+        p = P_D_k
+        demand_prob_arr.append(P_D_k)
+        k += 1
+
+    return np.array(demand_prob_arr)
+
+
+    
