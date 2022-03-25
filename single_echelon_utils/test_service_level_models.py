52fea2c2d82ebb5f11cb4d57ea2c81d44f5dd489
Alexander Larsson <al6313la-s@student.lu.se>
2022-03-25 14:13:58 +0100

    cases added to service level test

--- single_echelon_utils/test_service_level_models.py
+++ single_echelon_utils/test_service_level_models.py
@@ -13,6 +13,18 @@ def test_fill_rate_adj_normal(R, Q, mean_normal, std_dev_normal, demand_prob_arr
     normal = undershoot_adjustment_normal(R, Q, mean_normal, std_dev_normal, demand_prob_array)
     print(normal)
 
+# Testing service level compound poisson demand
+def test_fill_rate_compound_poisson():
+    pass
+
+# Testing service level poisson demand
+def test_fill_rate_poisson():
+    pass
+
+# Testing service level compound poisson demand
+def test_ready_rate_discrete():
+    pass
+
 def main():
     test_fill_rate_adj_normal(14, 6, 7.84, 5.3, #insert array)
     test_fill_rate_normal(14, 6, 7.84, 5.3)
